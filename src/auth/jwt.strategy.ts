47553773a28a8742df8eff8ac258fef4fb9adf76
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-29 14:12:20 +0300

    first commit

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'your_jwt_secret',
+      secretOrKey: 'jwt_secret',
     });
   }
 
